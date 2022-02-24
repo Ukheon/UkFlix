dd00b9218ebd9e1b53ca35faf00f0c17d1728c6a
ukheon <dnrgjs33@naver.com>
2022-02-25 07:55:42 +0900

    basename

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import styled from "styled-components";
 
 function App() {
     return (
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
             <Header />
             <Routes>
                 <Route path={"/"} element={<Home></Home>}></Route>
